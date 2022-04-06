5c692ee6ba5e099dce139183187a6e06600379cc
Vinícius <hello@marvig.dev>
2022-04-06 08:58:18 -0300

    Adicionar funções para manipular a API

---
+++ src/api/request.js
@@ -0,0 +1,54 @@
+import './types';
+
+const API_BASE = 'https://opentdb.com/api.php?';
+const TOKEN_BASE = 'https://opentdb.com/api_token.php?';
+const CATEGORY_BASE = 'https://opentdb.com/api_category.php';
+
+/**
+ * Retorna uma lista de questões da API.
+ *
+ * @param {QuestionReqOptions} options Opções da requisição.
+ * @returns {Promise<QuestionReqResponse>} Dados das perguntas
+ */
+async function getQuestions({ token, category, difficulty, amount, type }) {
+  const DEFAULT_AMOUNT = 5;
+
+  const params = new URLSearchParams();
+
+  params.set('amount', amount || DEFAULT_AMOUNT);
+  if (token) params.set('token', token);
+  if (category) params.set('category', category);
+  if (difficulty) params.set('difficulty', difficulty);
+  if (type) params.set('type', type);
+
+  const request = await fetch(API_BASE + params.toString());
+  const data = await request.json();
+  return data;
+}
+
+/**
+ * Retorna um token de jogo para o usuário.
+ *
+ * @returns {Promise<TokenResponse>} Dados da requisição com o token do usuário
+ */
+async function getToken() {
+  const params = new URLSearchParams();
+  params.set('command', 'request');
+
+  const request = await fetch(TOKEN_BASE + params.toString());
+  const data = await request.json();
+  return data;
+}
+
+/**
+ * Retorna uma lista de categorias
+ *
+ * @returns {Promise<Category>} Lista de categorias
+ */
+async function getCategories() {
+  const request = await fetch(CATEGORY_BASE);
+  const data = await request.json();
+  return data.trivia_categories;
+}
+
+export { getCategories, getQuestions, getToken };
