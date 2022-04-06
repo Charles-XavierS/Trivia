3ad81869dec6bfc7bdb513ec8eede3ecb19c555e
Vinícius <hello@marvig.dev>
2022-04-06 12:58:32 -0300

    Criar reducer do token

---
+++ src/redux/reducers/tokenReducer.js
@@ -0,0 +1,10 @@
+const initialState = undefined;
+
+const tokenReducer = (state = initialState, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default tokenReducer;
